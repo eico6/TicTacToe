92e22956931fb3a464be78daad5c12c5ee8b1a12
Eivind Hobrad Naasen <58335786+eico6@users.noreply.github.com>
2022-08-23 18:12:44 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,13 @@
-cls
+@echo off
+
+echo Running "tictactoe.py" using your default version:
+python --version
+echo ...
+echo.
+
+:: Note that the file path is not specified.
 python tictactoe.py
+echo.
+
+echo If something went wrong, check README.md.
+pause
